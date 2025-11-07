4559affda2bc9cce0ca532fed3c3ee5a37f83815
Xenon <joshua123.jdr@gmail.com>
2025-11-08 01:30:25 +0800

    chore: add temp form link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-				<h1 className="text-center m-auto text-4xl text-gray-700 font-bold">PhiliPC</h1>
+		<div className="flex min-h-screen items-center justify-center bg-gray-700 font-sans">
+			<main className="flex flex-col min-h-screen w-full max-w-3xl items-center justify-between py-10 px-16 sm:items-center">
+				<h1 className="text-center m-auto text-4xl text-zinc-50 font-bold">PhiliPC</h1>
+				<div className="flex gap-10">
+					<Link href="/login" className="hover:underline">Log in</Link>
+					<Link href="/signup" className="hover:underline"> Sign up</Link>
+				</div>
 			</main>
 		</div>
 	);
