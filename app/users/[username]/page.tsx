2da5d2c353826066b1acf2e42652307e4e98e337
Xenon <joshua123.jdr@gmail.com>
2025-11-18 13:31:33 +0800

    feat(ui): add web icon and user ratings

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -7,6 +7,7 @@ import userMock from '@/app/data/userMock.json';
 import Products from '@/app/components/Products';
 import { useParams } from 'next/navigation';
 import { Product } from '@/app/data/types';
+import { Star } from 'lucide-react';
 
 const UserPage: React.FC = () => {
     const { username } = useParams();
@@ -55,28 +56,41 @@ const UserPage: React.FC = () => {
     }));
 
     return (
-        <div className="dark:bg-gray-800">
+        <div className="min-h-screen dark:bg-gray-800">
             <Navigation />
             <div className="mx-auto max-w-7xl p-6">
                 {/* User Info */}
-                <div className="mb-6 flex items-center gap-4 rounded-2xl bg-linear-to-r from-[#003d4d]/85 to-[#0081b3]/85 px-8 py-5 shadow-xl dark:from-gray-900 dark:to-gray-900">
-                    <div className="relative h-28 w-28">
-                        <Image
-                            src={user.avatar}
-                            alt={user.name}
-                            fill
-                            className="rounded-full object-cover"
-                        />
-                    </div>
-
-                    <div className="flex flex-col gap-3 text-white">
-                        <div className="">
-                            <h1 className="text-2xl font-bold">{user.name}</h1>
-                            <p className="text-gray-400">@{user.username}</p>
+                <div className="mb-6 flex items-center justify-between gap-4 rounded-2xl bg-linear-to-r from-[#003d4d]/85 to-[#0081b3]/85 px-8 py-5 shadow-xl dark:from-gray-900 dark:to-gray-900">
+                    <div className="flex items-center gap-5">
+                        <div className="relative h-28 w-28">
+                            <Image
+                                src={user.avatar}
+                                alt={user.name}
+                                fill
+                                className="rounded-full object-cover"
+                            />
                         </div>
-                        <p className="mt-1 text-white">
-                            <strong>Location:</strong> {user.location}
-                        </p>
+
+                        <div className="flex flex-col gap-3 text-white">
+                            <div className="">
+                                <h1 className="text-2xl font-bold">{user.name}</h1>
+                                <p className="text-gray-400">@{user.username}</p>
+                            </div>
+                            <p className="mt-1 text-white">
+                                <strong>Location:</strong> {user.location}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-start gap-2">
+                        <Star className="h-8 w-8 text-amber-300" />
+                        <div className="flex flex-col">
+                            <span className="text-lg font-semibold text-amber-300">
+                                {user.rating}
+                            </span>
+                            <span className="hidden text-xs text-white sm:block">
+                                {user.reviews.length} review/s
+                            </span>
+                        </div>
                     </div>
                 </div>
 
@@ -84,7 +98,7 @@ const UserPage: React.FC = () => {
                 <div className="mb-4 flex border-b border-gray-300 dark:border-gray-500">
                     <button
                         onClick={() => setActiveTab('listings')}
-                        className={`px-4 py-2 font-semibold ${
+                        className={`px-4 py-2 font-semibold hover:cursor-pointer ${
                             activeTab === 'listings'
                                 ? 'border-b-2 border-blue-600 text-blue-600'
                                 : 'text-gray-600 dark:text-gray-400'
@@ -94,7 +108,7 @@ const UserPage: React.FC = () => {
                     </button>
                     <button
                         onClick={() => setActiveTab('reviews')}
-                        className={`px-4 py-2 font-semibold ${
+                        className={`px-4 py-2 font-semibold hover:cursor-pointer ${
                             activeTab === 'reviews'
                                 ? 'border-b-2 border-blue-600 text-blue-600'
                                 : 'text-gray-600 dark:text-gray-400'
@@ -133,15 +147,19 @@ const UserPage: React.FC = () => {
                                 {user.reviews.map((review) => (
                                     <div
                                         key={review.id}
-                                        className="rounded-lg border border-gray-300 p-4 shadow-sm"
+                                        className="rounded-lg border border-gray-300 p-4 shadow-sm dark:border-gray-600"
                                     >
                                         <div className="flex items-center justify-between">
-                                            <p className="font-semibold">{review.reviewer}</p>
+                                            <p className="font-semibold dark:text-white">
+                                                {review.reviewer}
+                                            </p>
                                             <p className="text-yellow-500">
                                                 {'⭐'.repeat(review.rating)}
                                             </p>
                                         </div>
-                                        <p className="mt-2 text-gray-800">{review.comment}</p>
+                                        <p className="mt-2 text-gray-800 dark:text-gray-300">
+                                            {review.comment}
+                                        </p>
                                         <p className="mt-1 text-sm text-gray-500">{review.date}</p>
                                     </div>
                                 ))}
