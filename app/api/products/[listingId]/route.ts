f2761638e224334fa0da4370dfd2fe75a58bd8c1
Roman <lopezromanaragorn@gmail.com>
2025-11-21 00:43:13 +0800

    feat(app): added fetching for user details from db

--- app/api/products/[listingId]/route.ts
+++ app/api/products/[listingId]/route.ts
@@ -1,6 +1,6 @@
 import { getSpecificProduct } from '@/app/lib/specificProduct';
 
-export async function GET(req, { params }) {
+export async function GET(req: Response, { params }: { params: { listingId: number } }) : Promise<Response> {
     const { listingId } = await params;
 
     console.log(listingId);
--- app/lib/products.ts
+++ app/lib/products.ts
@@ -1,9 +1,23 @@
 import { createConnection } from '@/app/lib/db';
+import { RowDataPacket } from 'mysql2/promise';
 
-export async function getProducts() {
+interface Product extends RowDataPacket {
+    listing_id : number;
+    seller_id : number;
+    category : string;
+    item_name : string; 
+    item_condition : string;
+    item_price : number;
+    item_description: string;
+    item_location : string;
+    is_avail: boolean;
+    full_name : string
+}
+
+export async function getProducts(): Promise<Product[]> {
     const db = await createConnection();
     const [products] =
-        await db.query(`SELECT products.*, CONCAT(users.first_name, " ", users.last_name) AS full_name 
+        await db.query<Product[]>(`SELECT products.*, CONCAT(users.first_name, " ", users.last_name) AS full_name 
                                       FROM products 
                                       JOIN users 
                                       ON products.seller_id = users.user_id;`);
